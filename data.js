63cbb13f6e40dab51346e6167af179be00efa954
sortrobotics <sortrobotics@gmail.com>
2023-06-19 18:19:50 +0530

    commit

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-19",
+      "name": "19",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": -0.018932885535626554,
+        "pitch": 0.01978007468247256,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.05174099841562452,
+          "pitch": 0.5840896185128148,
+          "title": "नेवासा",
+          "text": "अवस्थान&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
